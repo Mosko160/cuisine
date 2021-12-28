d6f11d149d2a66d03d3b0370e00ae124256149ed
Mosko160 <ftt.baptiste@gmail.com>
2021-12-28 14:37:58 +0100

    add css

--- html/js/getRecipe.js
+++ html/js/getRecipe.js
@@ -7,16 +7,16 @@ function getContent(){
         for(a=0;a!=data['liste'].length;a++){
             p = document.createElement('p');
             p.innerHTML = data['liste'][a]+' : '+data[data['liste'][a]]
-            document.getElementById('container').appendChild(p);
+            document.getElementById('containerQt').appendChild(p);
         }
     });
 
     $.get('ajax',{action:'getRecipe',id:recipeId},(data,status)=>{
         data = JSON.parse(JSON.parse(data));
-        for(a=1;a!=data.etapes+1;a++){
-            p = document.createElement('p');
-            p.innerHTML = data[a];
-            document.getElementById('container').appendChild(p);
+        p = document.createElement('p');
+        for(a=1;a!=data.etapes+1;a++){   
+            p.innerHTML += `<h3>Étape ${a} :</h3> ${data[a]}\n<br>`;
         }
+        document.getElementById('container').appendChild(p);
     });
 }
